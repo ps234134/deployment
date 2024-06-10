0c8024f4719e0a1440a24991196f842203f10216
tutai tran <ps234134@summacollege.nl>
2024-06-10 15:15:48 +0200

    portfolio testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,26 +10,26 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    // public function test_the_application_returns_a_successful_response(): void
-    // {
-    //     $response = $this->get('/');
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(200);
+    }
 
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_the_portfolio_view(): void
-{
-    $response = $this->get('/');
+//     public function test_the_application_returns_the_portfolio_view(): void
+// {
+//     $response = $this->get('/');
 
-    if (env('CI')) {
-        file_put_contents('test_output.txt', $response->getContent());
-    }
+//     if (env('CI')) {
+//         file_put_contents('test_output.txt', $response->getContent());
+//     }
 
-    $response->assertViewIs('portfolio');
-}
+//     $response->assertViewIs('portfolio');
+// }
 
 
 }
